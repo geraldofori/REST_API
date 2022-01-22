5a803869a498642587b82e5c7fb350c5459210ad
gerald <oforige@gmail.com>
2022-01-22 04:15:57 +0000

    created models

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGO_URI: 'mongodb://pinooo:7xiDlszcT6VgIReb@cluster0-shard-00-00.zhgpd.mongodb.net:27017,cluster0-shard-00-01.zhgpd.mongodb.net:27017,cluster0-shard-00-02.zhgpd.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-z7m3o9-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
+//7xiDlszcT6VgIReb
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const {MONGO_URI} = require('./config');
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('Hello from node');
-});
+//Connect to MongoDB
+mongoose.connect(MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(()=> console.log('MongoDB connected!'))
+    .catch(err => console.log(err));
 
 const PORT = process.env.PORT ||5000;
 
